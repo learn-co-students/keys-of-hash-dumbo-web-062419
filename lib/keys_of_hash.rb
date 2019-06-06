0f6bafd5ec2cb623435127457c0037be4b533c7c
lgm527 <laurellmccaffrey@gmail.com>
2019-06-06 17:18:29 -0400

    keys of hash and splat operator

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    result = []
+    self.each do |k, v|
+      arguments.each do |i|
+        if i == v
+          result << k
+        end
+      end
+    end
+    return result
   end
 end
