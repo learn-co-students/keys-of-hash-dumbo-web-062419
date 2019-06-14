d5f5bf2f01c51b9e5f34a5ebbbcbd9e853810e59
emanovic7 <e_owusu@live.com>
2019-06-14 11:55:51 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    final_array = []
+    each do |key, value|
+      if arguments.include?(value)
+        final_array << key
+      end
+    end
+    final_array
   end
+
 end
