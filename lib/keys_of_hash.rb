0dc07656fc226080a18fb97a013349fa07f938ad
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-16 09:48:56 -0400

    finished keys_of_hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
+  	key_arr = []
     # code goes here
+    arguments.each do |arg|
+    	self.each do |key, val|
+    		if val == arg
+    			key_arr << key
+    		end
+    	end
+    end
+    key_arr
   end
 end
