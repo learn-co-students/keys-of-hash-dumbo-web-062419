61106217f9f127af7b00528c308af779756554a9
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 02:00:59 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
   def keys_of(arguments)
     # code goes here
+    arguments.each do |keys|
+      puts "#{keys}:"
+  return keys
   end
-end
+  
+  def keys_of('Panama')
+    Panama.each do |animal|
+      animal << "red-footed tortoise"
+    end
+    animal 
+    end 
